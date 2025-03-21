5cea0d420404d645e259435af09c03e2e094197c
praveengak <arugilpraveen@gmail.com>
2025-03-21 22:30:49 +0530

    Created Entity(user,Booking and Room)

---
+++ src/main/java/com/praveen/FiveStarHotelManagement/entity/User.java
@@ -0,0 +1,66 @@
+package com.praveen.FiveStarHotelManagement.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotNull(message = "Email is Required")
+    @Column(unique = true)
+    private String email;
+
+    @NotNull(message = "Name is Required")
+    private String name;
+
+    @NotNull(message =  "Phone Number is required")
+    private String phoneNumber;
+    private String password;
+    private String role;
+
+    private List<Booking> bookings = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
