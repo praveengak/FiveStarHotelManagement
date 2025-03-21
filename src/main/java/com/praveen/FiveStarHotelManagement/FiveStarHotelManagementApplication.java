2fbf13abee5cc4bb537d8904e9519239d8f73cbc
praveengak <arugilpraveen@gmail.com>
2025-03-21 16:22:37 +0530

    Created setup for the HotelManagement Project

---
+++ src/main/java/com/praveen/FiveStarHotelManagement/FiveStarHotelManagementApplication.java
@@ -0,0 +1,13 @@
+package com.praveen.FiveStarHotelManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiveStarHotelManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiveStarHotelManagementApplication.class, args);
+	}
+
+}
